b4b6b0d2d7bd591053a8a87d464d02204012267c
Emre Eren <emreeren@gmail.com>
2016-08-23 20:09:07 +0300

    Import fixes

--- app/components/Order.jsx
+++ app/components/Order.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
-import {List, ListItem} from 'material-ui/List';
-import {GridList, GridTile} from 'material-ui/GridList';
-import Paper from 'material-ui/Paper';
 
 const styles = {
     root: {
