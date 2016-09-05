0b7f736927f179c212ec4cb4d56edb1311fed970
Emre Eren <emreeren@gmail.com>
2016-09-05 10:18:45 +0300

    Portion buttons fixed

---
+++ app/components/Portion.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+
+export default class Portion extends React.Component {
+    render() {
+        const {name, price, isSelected, onClick = () => { } } = this.props;
+        const style = {
+            'color': isSelected ? 'White' : 'Black',
+            'display':'inline-block'
+        };
+        const style2 = {
+            'height': 'auto'
+        };
+        return (
+            <RaisedButton
+                style= {style2}
+                className="portionButton"
+                backgroundColor={isSelected?'Red':'White'}
+                onClick={onClick.bind(null, name) }>
+                <div style={{'padding':'4px'}}>
+                    <span style={style}>{name}</span><br/>
+                    <span style={style}>{price.toFixed(2) }</span>
+                </div>
+            </RaisedButton>
+        );
+    }
+}
--- app/components/TicketTotal.jsx
+++ app/components/TicketTotal.jsx
@@ -1,15 +1,4 @@
 import React from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
-import {List, ListItem} from 'material-ui/List';
-import {GridList, GridTile} from 'material-ui/GridList';
-import Paper from 'material-ui/Paper';
-
-const styles = {
-    root: {
-        display: 'flex',
-        alignContent: 'stretch'
-    }
-};
 
 export default class TicketTotal extends React.Component {
     render() {
